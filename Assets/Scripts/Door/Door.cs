d01d9027edd26e6a833808d8d26d67a1fd077856
Mehmet Akif Genç <mehmetakifgenc3361@gmail.com>
2025-05-03 21:21:28 +0300

    Project 3d to 2D

--- Assets/Scripts/Door/Door.cs
+++ Assets/Scripts/Door/Door.cs
@@ -1,19 +1,19 @@
 using UnityEngine;
 
-public class Door : MonoBehaviour
+namespace abc
 {
-    
-
-
-
-    void Start()
+    public class Door : MonoBehaviour
     {
-        
-    }
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+            
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
     }
 }
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,95 +1,100 @@
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+namespace abc
 {
-    [Header("Movement")]
-    [SerializeField] private float maxSpeed = 8f;
-    [SerializeField] private float acceleration = 50f;
-    [SerializeField] private float deceleration = 70f;
 
-    [Header("Jump")]
-    [SerializeField] private float jumpForce = 15f;
-    [SerializeField] private float gravityScale = 3f;
-    [SerializeField] private float apexGravityScale = 1.5f;
-
-    [Header("Ground Detection")]
-    [SerializeField] private Transform groundCheck;
-    [SerializeField] private float groundCheckRadius = 0.1f;
-    [SerializeField] private LayerMask groundLayer;
-
-    [Header("Coyote & Buffer")]
-    [SerializeField] private float coyoteTime = 0.2f;
-    [SerializeField] private float jumpBufferTime = 0.15f;
-
-    private Rigidbody2D rb;
-    private float moveInput;
-    private bool jumpPressed;
-    private float coyoteCounter;
-    private float bufferCounter;
-    private bool isGrounded;
-
-    private void Awake()
+    public class PlayerController : MonoBehaviour
     {
-        rb = GetComponent<Rigidbody2D>();
-    }
+        [Header("Movement")]
+        [SerializeField] private float maxSpeed = 8f;
+        [SerializeField] private float acceleration = 50f;
+        [SerializeField] private float deceleration = 70f;
 
-    private void Update()
-    {
-        moveInput = Input.GetAxisRaw("Horizontal");
+        [Header("Jump")]
+        [SerializeField] private float jumpForce = 15f;
+        [SerializeField] private float gravityScale = 3f;
+        [SerializeField] private float apexGravityScale = 1.5f;
 
-        if (Input.GetButtonDown("Jump"))
-            bufferCounter = jumpBufferTime;
-        else
-            bufferCounter -= Time.deltaTime;
+        [Header("Ground Detection")]
+        [SerializeField] private Transform groundCheck;
+        [SerializeField] private float groundCheckRadius = 0.1f;
+        [SerializeField] private LayerMask groundLayer;
 
-        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+        [Header("Coyote & Buffer")]
+        [SerializeField] private float coyoteTime = 0.2f;
+        [SerializeField] private float jumpBufferTime = 0.15f;
 
-        if (isGrounded)
-            coyoteCounter = coyoteTime;
-        else
-            coyoteCounter -= Time.deltaTime;
+        private Rigidbody2D rb;
+        private float moveInput;
+        private bool jumpPressed;
+        private float coyoteCounter;
+        private float bufferCounter;
+        private bool isGrounded;
 
-        if (coyoteCounter > 0f && bufferCounter > 0f)
+
+        private void Awake()
         {
-            jumpPressed = true;
-            bufferCounter = 0f;
-            coyoteCounter = 0f;
+            rb = GetComponent<Rigidbody2D>();
         }
-    }
 
-    private void FixedUpdate()
-    {
-        // ivmelenme
-        float targetSpeed = moveInput * maxSpeed;
-        float speedDiff = targetSpeed - rb.linearVelocity.x;
-        float accelRate = (Mathf.Abs(targetSpeed) > 0.01f) ? acceleration : deceleration;
-        float movement = Mathf.Pow(Mathf.Abs(speedDiff) * accelRate, 0.9f) * Mathf.Sign(speedDiff);
-        rb.AddForce(Vector2.right * movement);
-
-        // gravity
-        if (rb.linearVelocity.y > 2f)
-            rb.gravityScale = gravityScale;
-        else if (rb.linearVelocity.y < -2f)
-            rb.gravityScale = gravityScale;
-        else
-            rb.gravityScale = apexGravityScale;
-
-        // jump
-        if (jumpPressed)
+        private void Update()
         {
-            rb.linearVelocity = new Vector2(rb.linearVelocity.x, 0f);
-            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
-            jumpPressed = false;
-        }
-    }
+            moveInput = Input.GetAxisRaw("Horizontal");
 
-    private void OnDrawGizmosSelected()
-    {
-        if (groundCheck != null)
+            if (Input.GetButtonDown("Jump"))
+                bufferCounter = jumpBufferTime;
+            else
+                bufferCounter -= Time.deltaTime;
+
+            isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+
+            if (isGrounded)
+                coyoteCounter = coyoteTime;
+            else
+                coyoteCounter -= Time.deltaTime;
+
+            if (coyoteCounter > 0f && bufferCounter > 0f)
+            {
+                jumpPressed = true;
+                bufferCounter = 0f;
+                coyoteCounter = 0f;
+            }
+        }
+
+        private void FixedUpdate()
         {
-            Gizmos.color = Color.cyan;
-            Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
-        }
-    }
+            // ivmelenme
+            float targetSpeed = moveInput * maxSpeed;
+            float speedDiff = targetSpeed - rb.linearVelocity.x;
+            float accelRate = (Mathf.Abs(targetSpeed) > 0.01f) ? acceleration : deceleration;
+            float movement = Mathf.Pow(Mathf.Abs(speedDiff) * accelRate, 0.9f) * Mathf.Sign(speedDiff);
+            rb.AddForce(Vector2.right * movement);
 
+            // gravity
+            if (rb.linearVelocity.y > 2f)
+                rb.gravityScale = gravityScale;
+            else if (rb.linearVelocity.y < -2f)
+                rb.gravityScale = gravityScale;
+            else
+                rb.gravityScale = apexGravityScale;
+
+            // jump
+            if (jumpPressed)
+            {
+                rb.linearVelocity = new Vector2(rb.linearVelocity.x, 0f);
+                rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+                jumpPressed = false;
+            }
+        }
+
+        private void OnDrawGizmosSelected()
+        {
+            if (groundCheck != null)
+            {
+                Gizmos.color = Color.cyan;
+                Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
+            }
+        }
+
+    }
 }
